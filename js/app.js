80f134f1b1f8ef43ae79f0f55e0756f339030c1a
jycurien <curien.jeanyves@gmail.com>
2020-06-13 16:53:28 +0200

    minor look changes

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const textareaElt = document.querySelector('#js-input-text');
     const parsedTextDiv = document.querySelector('#js-parsed-text');
 
-    textareaElt.addEventListener('keyup', function() {
-        const text = this.value;
+    textareaElt.addEventListener('keyup', () => {
+        const text = textareaElt.value;
         parsedTextDiv.innerHTML = marked(text);
     });
 
