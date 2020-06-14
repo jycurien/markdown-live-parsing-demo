4dcaa78b71eccf74b92e8789543a12dfb4aa8e2f
Jean-Yves Curien <jy.curien@laposte.net>
2020-06-14 11:05:38 +0200

    more webpack config

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,11 @@ import marked from 'marked';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    const textareaElt = document.querySelector('#js-input-text');
+    const inputTextTextarea = document.querySelector('#js-input-text');
     const parsedTextDiv = document.querySelector('#js-parsed-text');
 
-    textareaElt.addEventListener('keyup', () => {
-        const text = textareaElt.value;
+    inputTextTextarea.addEventListener('keyup', () => {
+        const text = inputTextTextarea.value;
         parsedTextDiv.innerHTML = marked(text);
     });
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    plugins: [new MiniCssExtractPlugin()],
     entry: './src/js/app.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/i,
+                test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
         ],
